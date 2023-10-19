746753e3f9cee18850aab517dc125163a22bc5cf
declan-rice-41 <kaheilaw0106@gmail.com>
2023-10-19 22:41:31 +0800

    fix: rm parseInt

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const development = {
   dialect: 'mysql',
   host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT),
+  port: process.env.DB_PORT,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
@@ -12,7 +12,7 @@ const development = {
 const production = {
   dialect: 'mysql',
   host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT),
+  port: process.env.DB_PORT,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
